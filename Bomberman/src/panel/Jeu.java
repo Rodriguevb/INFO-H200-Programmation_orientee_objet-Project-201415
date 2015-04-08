4c229f137205fdd6e3f491519ce5ad97f39cf3ad
soaz95 <sophia.azzagnuni@gmail.com>
2015-04-08 18:08:09 +0200

    Modifications

--- Bomberman/src/panel/Jeu.java
+++ Bomberman/src/panel/Jeu.java
@@ -17,7 +17,7 @@ public class Jeu extends JPanel implements KeyListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Plateau plateau = new Plateau();
+	private Plateau plateau = new Plateau(); // plateau
 	
 	
 	public Jeu(){

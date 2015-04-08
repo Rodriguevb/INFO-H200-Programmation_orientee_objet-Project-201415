c72eaddd751810fc0a450964f63fe1bf1507c1ad
soaz95 <sophia.azzagnuni@gmail.com>
2015-04-08 17:32:20 +0200

    Bouton_jouer

--- Bomberman/src/panel/Bouton_jouer.java
+++ Bomberman/src/panel/Bouton_jouer.java
@@ -6,9 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 import fenetre.Fenetre;
-import jeu.Controleur;
-import jeu.Plateau;
-import jeu.Vue;
 
 public class Bouton_jouer extends JButton implements ActionListener{
 	
@@ -21,7 +18,7 @@ public class Bouton_jouer extends JButton implements ActionListener{
 	
 	
 	public Bouton_jouer(Fenetre fenetre) {
-		super( "Jouer" );
+		super("Jouer");
 		this.fenetre = fenetre;
 		this.addActionListener(this); // mets le bouton sur Žcoute de la souris
 	}

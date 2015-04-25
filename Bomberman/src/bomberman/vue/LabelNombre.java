bfc16abbc075f304d0b0d7e16f3fba13dc1356dc
egruwe <egruwe@ulb.ac.be>
2015-04-25 22:08:48 +0200

    Amélioration de la zone Etat du Jeu

---
+++ Bomberman/src/bomberman/vue/LabelNombre.java
@@ -0,0 +1,52 @@
+package bomberman.vue;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import bomberman.controleur.Controleur;
+import bomberman.modele.Modele ;
+
+
+public class LabelNombre extends JLabel {
+	
+	private static final long serialVersionUID = 1L;
+	private Modele modele = null;
+	private Controleur controleur = null ;
+	private Integer personnageConcerne ;
+	private Integer objetConcerne ;
+	private String nbAffiche ;
+	
+	
+	public LabelNombre (int id, int objet , String nb, int y){
+		super(nb);
+		this.controleur = controleur ;
+		this.modele = modele ;
+		this.nbAffiche = nb ;
+		this.personnageConcerne = id;
+		this.objetConcerne = objet;	
+		this.setFont(new java.awt.Font("Comic Sans MS",1,15));
+		this.setForeground(new Color(175,175,175));
+		this.setBounds(100,y,30,30);		
+	}
+
+
+	public String getNbAffiche() {
+		return nbAffiche;
+	}
+
+
+	public Integer getPersonnageConcerne() {
+		return personnageConcerne;
+	}
+
+
+	public Integer getObjetConcerne() {
+		return objetConcerne;
+	}
+
+}
--- Bomberman/src/bomberman/vue/Points.java
+++ Bomberman/src/bomberman/vue/Points.java
@@ -6,8 +6,10 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Label;
 import java.awt.Point;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import bomberman.controleur.Controleur;
@@ -16,50 +18,73 @@ import bomberman.modele.Modele ;
 public class Points extends JPanel {
 	
 /**
-* La classe qui affiche les points et les bonus.
+* La classe qui affiche le nombre de vies et de bonus de chaque personnage.
 */
 	
 	private static final long serialVersionUID = 1L;
 	private Modele modele = null;
 	private Controleur controleur = null ;
+	private ArrayList<LabelNombre> listeLabels = new ArrayList<LabelNombre>();
 
 	public Points(Controleur controleur, Modele modele){
 		this.modele = modele ;
 		this.controleur = controleur ;
 		setLayout(null);
-		Creation_Trame();
 		this.setBackground(new Color(0,0,0));
+		CreationListeLabels();
+		CreationTrame();
 		
+		
+	
 	}
 	
-	public void Creation_Trame (){
+	public void CreationTrame (){
 	
 		Label label1 = new Label("Etat du jeu");
 		label1.setFont(new java.awt.Font("Comic Sans MS",1,40));
 		label1.setForeground(new Color(175,175,175));
 		label1.setBounds(60,30,300,50);
 		this.add(label1);
-		//for ( int id = 0; id < controleur.getNumberPersonnage(); ++id );
-			//Points.add(new Texte());
+		for (LabelNombre label : listeLabels){
+			this.add(label);
+		}
+	}	
+	
+	public void CreationListeLabels(){
+		
+		for (int i = 0 ; i < 4 ; i++){
+			for (int j = 0 ; j < 2 ; j++){
+			listeLabels.add(new LabelNombre(i, j, "", 140 + 150*i + 40 *j));
+			}
+		}
+	}
+		
+		
 
-}
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
-		this.setBackground(new Color(0,0,0));
 		
 		for ( int id = 0; id < controleur.getNumberPersonnage(); ++id ){
 
 			    int Nombre_Vies = modele.getPersonnage( id ).getNb_vies();
-			    g2d.drawImage(new ImageIcon("personnage1.png").getImage(), 50, 100+130*id, 80, 20, null);
-			    g2d.drawImage(new ImageIcon("bonus_vie.png").getImage(), 50, 140+130*id, 30, 30, null);
-			    g2d.drawImage(new ImageIcon("bonus_intensite1.png").getImage(), 50, 180+130*id, 30, 30, null);
-			    
-			    
+			    g2d.drawImage(new ImageIcon("personnage1.png").getImage(), 50, 100+150*id, 80, 30, null);
+			    g2d.drawImage(new ImageIcon("bonus_vie.png").getImage(), 50, 140+150*id, 30, 30, null);
+			    g2d.drawImage(new ImageIcon("bonus_intensite1.png").getImage(), 50, 180+150*id, 30, 30, null);
+			
+		for (LabelNombre label : listeLabels){
+			if (label.getPersonnageConcerne() == id && label.getObjetConcerne() == 0){
+				label.setText(Integer.toString(Nombre_Vies));
+				System.out.println(label.getNbAffiche());
+				
 			}
-		
+			
+		}
+		}
 	}
+	
+	
 	
 }
 

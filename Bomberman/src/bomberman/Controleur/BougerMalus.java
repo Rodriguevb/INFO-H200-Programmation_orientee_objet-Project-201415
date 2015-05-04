661843551a0f3593dbc8100a5febefc25d29e5fb
egruwe <egruwe@ulb.ac.be>
2015-05-04 17:38:46 +0200

    Pingouin aléatoire

--- Bomberman/src/bomberman/Controleur/BougerMalus.java
+++ Bomberman/src/bomberman/Controleur/BougerMalus.java
@@ -1,7 +1,9 @@
 package bomberman.controleur;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import bomberman.modele.Malus;
 import bomberman.modele.Modele;
@@ -17,7 +19,7 @@ public class BougerMalus implements ActionListener{
 	private Modele modele;
 	private int identifiant;
 	private boolean [] directionsPossibles = {false, false, false, false};
-	
+	private Point[] directions = {new Point (0,-1), new Point (1,0), new Point (0,1), new Point (-1, 0)};
 	
 	/**
 	 * Constructeur de la classe BougerMalus
@@ -48,16 +50,26 @@ public class BougerMalus implements ActionListener{
 		if (mal != null) { 
 		int x = mal.getX();
 		int y = mal.getY();
-		int[] dir_x = new int[] {1,-1,0,0};
-		int[] dir_y = new int[] {0,0,1,-1};
+		//int[] dir_x = new int[] {1,-1,0,0};
+		//int[] dir_y = new int[] {0,0,1,-1};
+
 		int v_x = mal.getV_x();
 		int v_y = mal.getV_y();
 		
 		changeDirectionsPossibles(x, y);
+		int n = randomNombre();
 		
-		if (controleur.estLibre(x+v_x, y+v_y)){
-			mal.setX(x+v_x);
-			mal.setY(y+v_y);
+		if (existeDirectionPossible()){
+		while (directionsPossibles[n] == false || faireDemiTour(v_x, v_y, n) == false){
+			n = randomNombre();}	
+			
+			x = x + directions[n].x ;
+			y = y + directions[n].y ;		
+			mal.setV_x(directions[n].x);
+			mal.setV_y(directions[n].y);
+			mal.setX(x);
+			mal.setY(y);
+			
 			if (modele.PersonnageSurCase(x, y)){
 				Personnage personnage = modele.getPersonnageSurPlateau(x, y);
 				if (personnage.getVivant() && !personnage.isGilet()){
@@ -68,16 +80,6 @@ public class BougerMalus implements ActionListener{
 				}
 			}
 		}
-		else {
-			while(!controleur.estLibre(x+v_x, y+v_y)){
-			int n = (int) (Math.random()*4);
-			v_x = dir_x[n];
-			v_y = dir_y[n];
-			}
-			mal.setV_x(v_x);
-			mal.setV_y(v_y);
-			
-		}
 		}
 		controleur.repaint();
 	
@@ -87,13 +89,48 @@ public class BougerMalus implements ActionListener{
 		
 		if (controleur.estLibre(x, y-1)){
 			directionsPossibles[0] = true ;}
+		else directionsPossibles[0] = false ;
+		
 		if (controleur.estLibre(x+1, y)){
 			directionsPossibles[1] = true ;}
+		else directionsPossibles[1] = false ;
+		
 		if (controleur.estLibre(x, y+1)){
 			directionsPossibles[2] = true ;}
-		if (controleur.estLibre(x-1, y)){
-			directionsPossibles[3] = true ;}	
+		else directionsPossibles[2] = false ;
 		
+		if (controleur.estLibre(x-1, y)){
+			directionsPossibles[3] = true ;}
+		else directionsPossibles[3] = false ;
+		
+	}
+	
+	public boolean existeDirectionPossible() {
+		Boolean existe = false ;
+		for (Boolean bool : directionsPossibles){
+			if (bool == true) existe = true ;
+		}
+		return existe ;
+	}
+	
+	public int randomNombre(){
+		Random r = new Random(); 
+		int n = r.nextInt(4);
+		return n ;
+	}
+	
+	public boolean faireDemiTour(int v_x, int v_y, int n){
+		boolean demiTour = true ;
+		int i = 0 ;
+		if (v_x == -directions[n].x && v_y == -directions[n].y) {
+			demiTour = false ;
+		
+			for (Boolean bool : directionsPossibles){
+			if (bool == false) i++ ;}
+			if (i == 3) demiTour = true ;
+		}
+		
+		return demiTour ;
 	}
 
 }
